6159f2b2eae3e05b4a934cfd062f3265a5ea9436
spences10 <spences10apps@gmail.com>
2018-09-30 06:38:02 +0100

    links

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -5,23 +5,21 @@ import NavStyles from './styles/navStyles'
 const Nav = () => {
   return (
     <NavStyles>
-
-    <Link href="/items">
-      <a>Items</a>
-    </Link>
-    <Link href="/sell">
-      <a>Sell</a>
-    </Link>
-    <Link href="/signup">
-      <a>Signup</a>
-    </Link>
-    <Link href="/orders">
-      <a>Orders</a>
-    </Link>
-    <Link href="/me">
-      <a>Account</a>
-    </Link>
-
+      <Link href="/items">
+        <a>Items</a>
+      </Link>
+      <Link href="/sell">
+        <a>Sell</a>
+      </Link>
+      <Link href="/signup">
+        <a>Signup</a>
+      </Link>
+      <Link href="/orders">
+        <a>Orders</a>
+      </Link>
+      <Link href="/me">
+        <a>Account</a>
+      </Link>
     </NavStyles>
   )
 }
