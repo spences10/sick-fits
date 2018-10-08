ad06e8e8104e4004f17d03d9bf189ae3dd4c5b55
spences10 <spences10apps@gmail.com>
2018-10-08 06:33:21 +0100

    items creation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,18 @@
-const mutations = {}
+const mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO Check if they are logged in
+
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      },
+      info
+    })
+
+    console.log(item)
+
+    return item
+  }
+}
 
 module.exports = mutations
