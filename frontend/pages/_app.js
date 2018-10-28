bffe925df8147d80bb2b6a7a7527d8a4ba5fabe3
spences10 <spences10apps@gmail.com>
2018-10-28 09:22:28 +0000

    use global style for styled-components v4

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,11 +1,13 @@
 import App, { Container } from 'next/app'
 import Page from '../components/Page'
+import { GlobalStyle } from '../components/Page'
 
 class MyApp extends App {
   render() {
     const { Component } = this.props
     return (
       <Container>
+        <GlobalStyle />
         <Page>
           <Component />
         </Page>
