168aa3aa94342b72e5a2e58164a4326050f5d47f
spences10 <spences10apps@gmail.com>
2018-11-18 18:41:03 +0000

    single items query

--- frontend/components/UpdateItem.js
+++ frontend/components/UpdateItem.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Mutation } from 'react-apollo'
+import { Mutation, Query } from 'react-apollo'
 import gql from 'graphql-tag'
 import Router from 'next/router'
 
@@ -8,34 +8,40 @@ import Error from './errorMessage'
 
 import formatMoney from '../lib/formatMoney'
 
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($id: ID!) {
+    item(where: { id: $id }) {
+      id
+      title
+      description
+      price
+    }
+  }
+`
+
 const UPDATE_ITEM_MUTATION = gql`
   mutation UPDATE_ITEM_MUTATION(
-    $title: String!
-    $description: String!
-    $price: Int!
-    $image: String
-    $largeImage: String
+    $id: ID!
+    $title: String
+    $description: String
+    $price: Int
   ) {
-    createItem(
+    updateItem(
+      id: $id
       title: $title
       description: $description
       price: $price
-      image: $image
-      largeImage: $largeImage
     ) {
       id
+      title
+      description
+      price
     }
   }
 `
 
 class UpdateItem extends React.Component {
-  state = {
-    title: '',
-    description: '',
-    image: '',
-    largeImage: '',
-    price: 0
-  }
+  state = {}
 
   handleChange = e => {
     const { name, type, value } = e.target
@@ -45,71 +51,83 @@ class UpdateItem extends React.Component {
     })
   }
 
+  updateItem = async (e, updateItemMutation) => {
+    e.preventDefault()
+    console.log('=====================')
+    console.log('Updating item!!')
+    console.log(this.state)
+    console.log('=====================')
+    const res = await updateItemMutation({
+      variables: {
+        id: this.state.id,
+        ...this.state
+      }
+    })
+    console.log('done!')
+  }
+
   render() {
     return (
-      <Mutation
-        mutation={UPDATE_ITEM_MUTATION}
-        variables={this.state}
+      <Query
+        query={SINGLE_ITEM_QUERY}
+        variables={{ id: this.props.id }}
       >
-        {(createItem, { loading, error }) => (
-          <Form
-            onSubmit={async e => {
-              // stop the form from submitting
-              e.preventDefault()
-              // call the mutation ✨✨✨✨
-              const res = await createItem()
-              // change to item page
-              Router.push({
-                pathname: '/item',
-                query: { id: res.data.createItem.id }
-              })
-              console.log('=====================')
-              console.log(res)
-              console.log('=====================')
-            }}
-          >
-            <Error error={error} />
-            <fieldset disabled={loading} aria-busy={loading}>
-              <label htmlFor="title">
-                Title
-                <input
-                  type="text"
-                  id="title"
-                  name="title"
-                  placeholder="Title"
-                  required
-                  value={this.state.title}
-                  onChange={this.handleChange}
-                />
-              </label>
-              <label htmlFor="price">
-                Price
-                <input
-                  type="number"
-                  id="price"
-                  name="price"
-                  placeholder="Price "
-                  required
-                  value={this.state.price}
-                  onChange={this.handleChange}
-                />
-              </label>
-              <label htmlFor="description">
-                Description
-                <textarea
-                  id="description"
-                  name="description"
-                  placeholder="Enter a Description"
-                  required
-                  value={this.state.description}
-                  onChange={this.handleChange}
-                />
-              </label>
-              <button type="submit">Submit</button>
-            </fieldset>
-          </Form>
-        )}
-      </Mutation>
+        {({ data, loading }) => {
+          if (loading) return <p>Loading...</p>
+          if (!data.item)
+            return <p>No Item Found for ID: {this.props.id}</p>
+          return (
+            <Mutation
+              mutation={UPDATE_ITEM_MUTATION}
+              variables={this.state}
+            >
+              {(updateItem, { loading, error }) => (
+                <Form onSubmit={e => this.updateItem(e, updateItem)}>
+                  <Error error={error} />
+                  <fieldset disabled={loading} aria-busy={loading}>
+                    <label htmlFor="title">
+                      Title
+                      <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        placeholder="Title"
+                        required
+                        defaultValue={data.item.title}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                    <label htmlFor="price">
+                      Price
+                      <input
+                        type="number"
+                        id="price"
+                        name="price"
+                        placeholder="Price "
+                        required
+                        defaultValue={data.item.price}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                    <label htmlFor="description">
+                      Description
+                      <textarea
+                        id="description"
+                        name="description"
+                        placeholder="Enter a Description"
+                        required
+                        defaultValue={data.item.description}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                    <button type="submit">Save Changes</button>
+                  </fieldset>
+                </Form>
+              )}
+            </Mutation>
+          )
+        }}
+      </Query>
     )
   }
 }
