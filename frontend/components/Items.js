7635e948f8094649c350533e445e41c8543252a7
spences10 <spences10apps@gmail.com>
2018-11-18 09:01:34 +0000

    add items component

---
+++ frontend/components/Items.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Query } from 'react-apollo'
+
+class Items extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <p>jsx goes here</p>
+      </React.Fragment>
+    )
+  }
+}
+
+export default Items
