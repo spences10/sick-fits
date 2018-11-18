bdfb5ba2d792cd6098895ca0109dbd26605642b1
spences10 <spences10apps@gmail.com>
2018-11-18 17:47:05 +0000

    update mutations an queries for editing items

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -14,6 +14,12 @@ const Mutations = {
     console.log(item)
 
     return item
+  },
+  updateItem(parent, args, ctx, info) {
+    // take a copy of the updates
+    const updates = { ...args }
+    // remove ID from there
+    delete updates.id
   }
 }
 
