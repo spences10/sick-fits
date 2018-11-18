02183bbd3932794c6420d7dd6f4908c5d0a69e6c
spences10 <spences10apps@gmail.com>
2018-11-18 14:19:06 +0000

    load images

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -45,6 +45,22 @@ class CreateItem extends React.Component {
     })
   }
 
+  uploadFile = async e => {
+    const files = e.target.files
+    const data = new FormData()
+    data.append('file', files[0])
+    data.append('upload_preset', 'sickfits')
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/defkmsrpw/image/upload',
+      { method: 'POST', body: data }
+    )
+    const file = await res.json()
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    })
+  }
+
   render() {
     return (
       <Mutation
@@ -70,6 +86,17 @@ class CreateItem extends React.Component {
           >
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
+                />
+              </label>
               <label htmlFor="title">
                 Title
                 <input
