c0f854f655b659cdafe15e29c21aad821393a8f6
spences10 <spences10apps@gmail.com>
2018-11-19 06:38:18 +0000

    add gql mutation

--- frontend/components/DeleteItem.js
+++ frontend/components/DeleteItem.js
@@ -1,4 +1,14 @@
 import React from 'react'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const DELETE_ITEM_MUTATION = gql`
+  mutation DELETE_ITEM_MUTATION($id: ID!) {
+    deleteItem(id: $id) {
+      id
+    }
+  }
+`
 
 class DeleteItem extends React.Component {
   render() {
