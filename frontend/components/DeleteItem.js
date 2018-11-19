eca359a43bbd476e396012dbcc1f903453675356
spences10 <spences10apps@gmail.com>
2018-11-19 06:53:47 +0000

    write delete items mutation and page refresh

--- frontend/components/DeleteItem.js
+++ frontend/components/DeleteItem.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
 
+import { ALL_ITEMS_QUERY } from './Items'
+
 const DELETE_ITEM_MUTATION = gql`
   mutation DELETE_ITEM_MUTATION($id: ID!) {
     deleteItem(id: $id) {
@@ -11,11 +13,40 @@ const DELETE_ITEM_MUTATION = gql`
 `
 
 class DeleteItem extends React.Component {
+  update = (cache, payload) => {
+    // manually update the cache
+    // so it matches the server
+    // read the cache
+    const data = cache.readQuery({ query: ALL_ITEMS_QUERY })
+    console.log('=====================')
+    console.log(data)
+    console.log('=====================')
+    // filter for deleted id
+    data.items = data.items.filter(
+      item => item.id !== payload.data.deleteItem.id
+    )
+    // put items back
+    cache.writeQuery({ query: ALL_ITEMS_QUERY, data })
+  }
   render() {
     return (
-      <React.Fragment>
-        <button>{this.props.children}</button>
-      </React.Fragment>
+      <Mutation
+        mutation={DELETE_ITEM_MUTATION}
+        variables={{ id: this.props.id }}
+        update={this.update}
+      >
+        {(deleteItem, { error }) => (
+          <button
+            onClick={() => {
+              if (confirm('Are you sure? Delete, right?')) {
+                deleteItem()
+              }
+            }}
+          >
+            {this.props.children}
+          </button>
+        )}
+      </Mutation>
     )
   }
 }
