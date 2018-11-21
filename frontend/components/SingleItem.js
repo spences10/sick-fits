5c15f0c4772d221eb4b8cdf620f825dc116a5d4a
spences10 <spences10apps@gmail.com>
2018-11-21 06:36:25 +0000

    add query and styled component

--- frontend/components/SingleItem.js
+++ frontend/components/SingleItem.js
@@ -1,5 +1,19 @@
 import React from 'react'
 import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+import styled from 'styled-components'
+
+import Error from './errorMessage'
+
+const SingleItemStyle = styled.div`
+  min-width: 1200px;
+  margin: 2ren auto;
+  box-shadow: ${({ theme }) => theme.bs};
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  min-height: 800px;
+`
 
 const SINGLE_ITEM_QUERY = gql`
   query SINGLE_ITEM_QUERY($id: ID!) {
@@ -15,9 +29,25 @@ const SINGLE_ITEM_QUERY = gql`
 class SingleItem extends React.Component {
   render() {
     return (
-      <React.Fragment>
-        <p>Single Item Component</p>
-      </React.Fragment>
+      <Query
+        query={SINGLE_ITEM_QUERY}
+        variables={{
+          id: this.props.id
+        }}
+      >
+        {(error, loading, data) => {
+          if (error) return <Error error={error} />
+          if (loading) return <p>Loading...</p>
+          if (!data.item)
+            return <p>No item found for {this.props.id}</p>
+          const item = data.item
+          return (
+            <SingleItemStyle>
+              <img src={item.largeImage} alt={item.title} />
+            </SingleItemStyle>
+          )
+        }}
+      </Query>
     )
   }
 }
