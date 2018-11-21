b36b3b529ef72ac46e847595de909cbe667d23d4
spences10 <spences10apps@gmail.com>
2018-11-21 06:36:15 +0000

    pass props

--- frontend/pages/item.js
+++ frontend/pages/item.js
@@ -3,7 +3,7 @@ import SingleItem from '../components/SingleItem'
 const Item = props => {
   return (
     <React.Fragment>
-      <SingleItem />
+      <SingleItem id={props.query.id} />
     </React.Fragment>
   )
 }
