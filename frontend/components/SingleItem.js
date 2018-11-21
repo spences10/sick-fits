148197e6e567a7c4605b5d17f901b0199765d4fa
spences10 <spences10apps@gmail.com>
2018-11-21 06:02:30 +0000

    move query to component

--- frontend/components/SingleItem.js
+++ frontend/components/SingleItem.js
@@ -1,4 +1,16 @@
 import React from 'react'
+import gql from 'graphql-tag'
+
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($id: ID!) {
+    item(where: { id: $id }) {
+      id
+      title
+      description
+      largeImage
+    }
+  }
+`
 
 class SingleItem extends React.Component {
   render() {
--- frontend/pages/item.js
+++ frontend/pages/item.js
@@ -1,16 +1,4 @@
 import SingleItem from '../components/SingleItem'
-import gql from 'graphql-tag'
-
-const SINGLE_ITEM_QUERY = gql`
-  query SINGLE_ITEM_QUERY($id: ID!) {
-    item(where: { id: $id }) {
-      id
-      title
-      description
-      largeImage
-    }
-  }
-`
 
 const Item = props => {
   return (
