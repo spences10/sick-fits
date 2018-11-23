86a6293f90e380074c8cfd42a465654b085cde3c
spences10 <spences10apps@gmail.com>
2018-11-23 06:44:26 +0000

    pagination links

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
+import Head from 'next/head'
+import Link from 'next/link'
 
 import PaginationStyles from './styles/paginationStyles'
 
@@ -18,16 +20,49 @@ const PAGINATION_QUERY = gql`
 
 const Pagination = props => {
   return (
-    <PaginationStyles>
-      <Query query={PAGINATION_QUERY}>
-        {({ data, loading, error }) => {
-          if (loading) return <p>Loading...</p>
-          const count = data.itemsConnection.aggregate.count
-          const pages = Math.ceil(count / perPage)
-          return <p>Page 1 of {pages}</p>
-        }}
-      </Query>
-    </PaginationStyles>
+    <Query query={PAGINATION_QUERY}>
+      {({ data, loading, error }) => {
+        if (loading) return <p>Loading...</p>
+        const count = data.itemsConnection.aggregate.count
+        const pages = Math.ceil(count / perPage)
+        const page = props.page
+        return (
+          <PaginationStyles>
+            <Head>
+              <title>
+                Sick Fits! | Page {page} of {pages}
+              </title>
+            </Head>
+            <Link
+              prefetch
+              href={{
+                pathname: 'items',
+                query: { page: page - 1 }
+              }}
+            >
+              <a className="prev" aria-disabled={page <= 1}>
+                ⬅ Prev
+              </a>
+            </Link>
+            <p>
+              Page {props.page} of {pages}
+            </p>
+            <p>{count} Items Total</p>
+            <Link
+              prefetch
+              href={{
+                pathname: 'items',
+                query: { page: page + 1 }
+              }}
+            >
+              <a className="next" aria-disabled={page >= pages}>
+                ➡ Next
+              </a>
+            </Link>
+          </PaginationStyles>
+        )
+      }}
+    </Query>
   )
 }
 
