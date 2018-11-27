6682d930afdeed5e8677bd789e511724dce12ebf
spences10 <spences10apps@gmail.com>
2018-11-27 06:50:29 +0000

    add signin component

--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 
 import Signup from '../components/Signup'
+import Signin from '../components/Signin'
 
 const Columns = styled.div`
   display: grid;
@@ -12,7 +13,7 @@ const SignupPage = props => {
   return (
     <Columns>
       <Signup />
-      <Signup />
+      <Signin />
       <Signup />
     </Columns>
   )
