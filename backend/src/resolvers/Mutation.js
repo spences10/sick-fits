1873f17783391e77e12b1f7368f506515cc1bb97
spences10 <spences10apps@gmail.com>
2018-11-27 06:33:09 +0000

    create signin mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -70,6 +70,30 @@ const Mutations = {
     })
     // return the user
     return user
+  },
+  async signin(parent, { email, password }, ctx, info) {
+    // 1. Check if there's a user with that email
+    const user = await ctx.db.query.user({ where: { email } })
+    if (!user) {
+      throw new Error(`No such user found for email: ${email}`)
+    }
+    // 2. Check if password is correct
+    const valid = await bcrypt.compare(password, user.password)
+    if (!valid) {
+      throw new Error('Invalid password yo!')
+    }
+    // 3. Generate JWT
+    const token = jwt.sign(
+      { userId: user.id },
+      process.env.APP_SECRET
+    )
+    // 4. Set the cookie with the token
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // one year
+    })
+    // 5. return the user
+    return user
   }
 }
 
