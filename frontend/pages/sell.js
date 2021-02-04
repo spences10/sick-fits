1f7c12693b8d58efd254cc5af3353d70fb9baaf8
spences10 <spences10apps@gmail.com>
2021-02-04 18:44:39 +0000

    create product

--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,7 +1,9 @@
+import CreateProduct from '../components/create-product'
+
 export default function SellPage() {
   return (
     <div>
-      <p>Hello!</p>
+      <CreateProduct />
     </div>
   )
 }
