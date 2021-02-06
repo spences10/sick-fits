e83925768724226994400f3d635fbb80506be161
spences10 <spences10apps@gmail.com>
2021-02-06 13:57:03 +0000

    create products

--- frontend/components/create-product.js
+++ frontend/components/create-product.js
@@ -2,6 +2,7 @@ import { useMutation } from '@apollo/client'
 import gql from 'graphql-tag'
 import useForm from '../lib/use-form'
 import DisplayError from './ErrorMessage'
+import { ALL_PRODUCTS_QUERY } from './products'
 import Form from './styles/Form'
 
 const CREATE_PRODUCT_MUTATION = gql`
@@ -40,14 +41,13 @@ export default function CreateProduct() {
     CREATE_PRODUCT_MUTATION,
     {
       variables: inputs,
+      refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
     }
   )
   return (
     <Form
       onSubmit={async e => {
         e.preventDefault()
-        console.log(inputs)
-        // Submit the inputfields to the backend:
         await createProduct()
         clearForm()
       }}
